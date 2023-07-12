87e2e4acefe5455b47024ff8065a7234cbe9602e
s1F102103303 <s1F102103303@iniad.org>
2023-07-12 23:48:46 +0900

    音声再生のプログラム

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,41 +17,43 @@ const Home: NextPage = () => {
       setDeniedSound(new Audio('/fail.mp3'))
     }
   }, [])
-  // useEffect(() => {
-  //   const postData = async () => {
-  //     await fetch('https://', {
-  //       method: 'POST',
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //       },
-  //       body: `${JSON.stringify(result)}`,
-  //     })
-  //       .then(async (response) => {
-  //         const data: ApiResponse = await response.json() // レスポンスをJSON形式で取得します。
-
-  //         if (data.success) {
-  //           // 成功時
-  //           if (allowedSound) {
-  //             allowedSound.play()
-  //           }
-  //         } else {
-  //           // 失敗時（入館許可がない場合）
-  //           if (deniedSound) {
-  //             deniedSound.play()
-  //           }
-  //         }
-  //       })
-  //       .catch(() => {
-  //         // エラーが発生した場合（通信エラーなど）
-  //         if (deniedSound) {
-  //           deniedSound.play()
-  //         }
-  //       })
-  //   }
-  //   if (result !== '') {
-  //     postData()
-  //   }
-  // }, [result, allowedSound, deniedSound])
+  useEffect(() => {
+    const postData = async () => {
+      const sendResult = JSON.parse(result)
+      sendResult['floorId'] = 2
+      if (process.env.NEXT_PUBLIC_SERVER_URL)
+        await fetch(process.env.NEXT_PUBLIC_SERVER_URL, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: `${JSON.stringify(sendResult)}`,
+        })
+          .then(async (response) => {
+            const data: ApiResponse = await response.json() // レスポンスをJSON形式で取得します。
+            if (data.success) {
+              // 成功時
+              if (allowedSound) {
+                allowedSound.play()
+              }
+            } else {
+              // 失敗時（入館許可がない場合）
+              if (deniedSound) {
+                deniedSound.play()
+              }
+            }
+          })
+          .catch(() => {
+            // エラーが発生した場合（通信エラーなど）
+            if (deniedSound) {
+              deniedSound.play()
+            }
+          })
+    }
+    if (result !== '') {
+      postData()
+    }
+  }, [result, allowedSound, deniedSound])
   return (
     <div>
       <QrReader setResult={setResult} onRequestClose={() => null} />
